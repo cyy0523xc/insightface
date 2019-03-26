a1da9888ddcf0759a1ad70e754b1140caa807705
alex <cyy0523xc@gmail.com>
2019-03-26 15:15:51 +0800

    start

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+# 启动
+# Author: alex
+# Created Time: 2019年03月26日 星期二 15时13分07秒
+docker run --rm -ti --name insightface \
+    -v /var/www/insightface/models:/models \
+    -v /var/www/github.com/insightface:/faces \
+    $1 /bin/bash
