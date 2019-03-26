0d1598c1870aac88f6fa4d763b6b4625ce544df7
alex <cyy0523xc@gmail.com>
2019-03-26 17:51:21 +0800

    test

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ python3 test.py --model /models/gamodel-r50/model,0 \
     --image-file ../tests/lldq01.jpeg
 """
 docker run --rm -ti --runtime=nvidia --name insightface \
+    -p 20920:20920 \
     -v /var/www/insightface/models:/models \
     -v /var/www/github.com/insightface:/faces \
     "$1" /bin/bash
