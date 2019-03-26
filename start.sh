9c567cca2fe0feb15511a9a346742e1bf196f558
alex <cyy0523xc@gmail.com>
2019-03-26 15:21:26 +0800

    start

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # 启动
 # Author: alex
 # Created Time: 2019年03月26日 星期二 15时13分07秒
-docker run --rm -ti --runtime=nvdia --name insightface \
+docker run --rm -ti --runtime=nvidia --name insightface \
     -v /var/www/insightface/models:/models \
     -v /var/www/github.com/insightface:/faces \
     "$1" /bin/bash
