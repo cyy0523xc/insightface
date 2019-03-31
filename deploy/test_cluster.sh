9af706e07011565de6c8eabb501ce531ed0a0acd
alex <cyy0523xc@gmail.com>
2019-03-31 23:23:37 +0800

    cluster

--- deploy/test_cluster.sh
+++ deploy/test_cluster.sh
@@ -5,4 +5,4 @@
 # Created Time: 2019年03月27日 星期三 14时24分40秒
 
 curl -XPOST 192.168.80.241:20920/cluster \
-    -d '{"path_dir": "'"$1"'", "algo": "'$2'", "k": '$3', "face_score": 0.9995}'
+    -d '{"path_dir": "'"$1"'", "algo": "'$2'", "k": '$3', "face_score": 0.9999}'
