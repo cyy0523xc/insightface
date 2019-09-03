6444e296711ab602b4f17f2d1fbf0a94e6f5af2d
alex <cyy0523xc@gmail.com>
2019-09-03 11:45:50 +0800

    retinaface

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ docker run --rm -ti --runtime=nvidia --name insightface \
     -p 20920:20920 \
     -v /var/www/face_models:/models \
     -v /var/www/tmp/faces:/var/www/tmp/faces \
-    -v /var/www/github.com/insightface:/faces \
+    -v `pwd`:/faces \
     -e MXNET_CUDNN_AUTOTUNE_DEFAULT=0 \
     -e PYTHONIOENCODING=utf-8 \
     -w /faces \
