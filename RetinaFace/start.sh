92ad8e3c2f6b2fc18c57f3862861856a3a65de1f
alex <cyy0523xc@gmail.com>
2019-09-04 09:55:38 +0800

    retinaface

--- RetinaFace/start.sh
+++ RetinaFace/start.sh
@@ -15,7 +15,7 @@ docker rm -f ibbd-face
 docker run --rm -d --runtime=nvidia --name ibbd-face \
     -p 20920:20920 \
     -v /var/www/face_models:/models \
-    -v /var/www/tmp/faces:/var/www/tmp/faces \
+    -v `pwd`/../tests:/faces/tests \
     -v `pwd`:/faces \
     -e MXNET_CUDNN_AUTOTUNE_DEFAULT=0 \
     -e PYTHONIOENCODING=utf-8 \
