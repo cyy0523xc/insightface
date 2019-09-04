0bfe31720ce0a1a9de5e6d367ccde65d6b07a261
alex <cyy0523xc@gmail.com>
2019-09-04 10:00:05 +0800

    retinaface

--- RetinaFace/start.sh
+++ RetinaFace/start.sh
@@ -11,11 +11,13 @@ if [ $# -le 2 ]; then
 fi
 echo "Command: $cmd"
 
+tests=`pwd`
+tests=${tests%/*}/tests
 docker rm -f ibbd-face
 docker run --rm -d --runtime=nvidia --name ibbd-face \
     -p 20920:20920 \
     -v /var/www/face_models:/models \
-    -v `pwd`/../tests:/faces/tests \
+    -v "$tests":/faces/tests \
     -v `pwd`:/faces \
     -e MXNET_CUDNN_AUTOTUNE_DEFAULT=0 \
     -e PYTHONIOENCODING=utf-8 \
